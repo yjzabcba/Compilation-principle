08a25a5b0c9475e7ea379ecdb10550c3e30c4d5a
zjy <zhou10426@gmail.com>
2025-02-19 12:35:22 +0800

    2021201812 function3_1

--- 2021201812.c
+++ 2021201812.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <limits.h>
 int main() {
     printf("This is the first lab!\n");
     printf("This is the second lab!\n");
+    printf("Sum: %d\n", sum);
     return 0;
 }
